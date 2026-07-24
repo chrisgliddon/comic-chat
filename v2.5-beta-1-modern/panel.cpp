63e0d038d892b24045732cdd6e2efa5ceed97130
Chris Gliddon <chris@nicely.ca>
2026-07-24 13:11:42 -0700

    Add granular logging in LayoutAvatars + FetchSpeaker + SetNeutral to pinpoint NULL recs

--- v2.5-beta-1-modern/panel.cpp
+++ v2.5-beta-1-modern/panel.cpp
@@ -622,7 +622,36 @@ CBody *CPanel::FetchSpeaker(UINT uID)
 	}
 
 	CAvatarX *av = GetAvatar(uID);
+#ifdef ORACLE_HARNESS
+	{
+		fprintf(stderr, "ORACLE: FetchSpeaker(avID=%d) av=%p m_body=%p class=%d\n",
+			uID, (void*)av, (void*)av->m_body, av->m_body ? av->m_body->GetClass() : -1);
+		if (av->m_body) {
+			if (av->m_body->GetClass() == BC_BODYDOUBLE) {
+				CBodyDouble *bd = (CBodyDouble*)av->m_body;
+				fprintf(stderr, "ORACLE:   m_body DOUBLE faceRec=%p torsoRec=%p\n",
+					(void*)bd->m_faceRec, (void*)bd->m_torsoRec);
+			} else {
+				CBodySingle *bs = (CBodySingle*)av->m_body;
+				fprintf(stderr, "ORACLE:   m_body SINGLE bodyRec=%p\n", (void*)bs->m_bodyRec);
+			}
+		}
+	}
+#endif
 	CBody *bdy = av->m_body->Clone();
+#ifdef ORACLE_HARNESS
+	{
+		fprintf(stderr, "ORACLE:   cloned bdy=%p class=%d\n", (void*)bdy, bdy->GetClass());
+		if (bdy->GetClass() == BC_BODYDOUBLE) {
+			CBodyDouble *bd = (CBodyDouble*)bdy;
+			fprintf(stderr, "ORACLE:   clone DOUBLE faceRec=%p torsoRec=%p\n",
+				(void*)bd->m_faceRec, (void*)bd->m_torsoRec);
+		} else {
+			CBodySingle *bs = (CBodySingle*)bdy;
+			fprintf(stderr, "ORACLE:   clone SINGLE bodyRec=%p\n", (void*)bs->m_bodyRec);
+		}
+	}
+#endif
 	av->RecordBody(bdy);
 
 	m_bodies.AddTail(bdy);
@@ -740,6 +769,10 @@ void CUnitPanel::LayoutAvatars() {
 	int bdyCount = 0, bdyWidth = 0, sumWidth = 0, maxNorm = 0;
 	int nBodies = m_bodies.GetCount();
 
+#ifdef ORACLE_HARNESS
+	fprintf(stderr, "ORACLE: LayoutAvatars ENTER nBodies=%d\n", nBodies);
+	fflush(stderr);
+#endif
 	// for now, lay them out in order
 	ASSERT(nBodies > 0 && nBodies < MAXBDYPERFRAME);
 	int maxBodyHeight = (int)(CUnitPanelPage::m_unitHeight / 1.9);
@@ -757,12 +790,38 @@ void CUnitPanel::LayoutAvatars() {
 
 	m_bodies.RemoveAll();
 
+#ifdef ORACLE_HARNESS
+	fprintf(stderr, "ORACLE: LayoutAvatars speakers=%d, calling OrderAvatars\n", bdyCount);
+	fflush(stderr);
+#endif
 	OrderAvatars(bRecs, bdyCount, placed);
 	ASSERT(bdyCount > 0);
 
+#ifdef ORACLE_HARNESS
+	fprintf(stderr, "ORACLE: LayoutAvatars OrderAvatars done, bdyCount=%d\n", bdyCount);
+	fflush(stderr);
+#endif
 	for (int i = 0; i < bdyCount; i++) {
 		CBody *b = ((CBodyRecord *)(placed[i]))->m_body;
+#ifdef ORACLE_HARNESS
+		{
+			fprintf(stderr, "ORACLE: LayoutAvatars i=%d body=%p class=%d\n", i, (void*)b, b->GetClass());
+			fflush(stderr);
+			if (b->GetClass() == BC_BODYDOUBLE) {
+				CBodyDouble *bd = (CBodyDouble*)b;
+				fprintf(stderr, "ORACLE:   DOUBLE faceRec=%p torsoRec=%p\n", (void*)bd->m_faceRec, (void*)bd->m_torsoRec);
+			} else {
+				CBodySingle *bs = (CBodySingle*)b;
+				fprintf(stderr, "ORACLE:   SINGLE bodyRec=%p\n", (void*)bs->m_bodyRec);
+			}
+			fflush(stderr);
+		}
+#endif
 		b->GetDimInfo(width[i], height[i], normHeight[i], headHeight[i], bitArrowX);
+#ifdef ORACLE_HARNESS
+		fprintf(stderr, "ORACLE: LayoutAvatars i=%d GetDimInfo done\n", i);
+		fflush(stderr);
+#endif
 		arrowX[i] = ((double) bitArrowX) / width[i];					// initially store arrows as percentage of width from left
 		maxNorm = max(maxNorm, normHeight[i]);
 	}
